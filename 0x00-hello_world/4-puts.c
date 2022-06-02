f70e25980bfcf1d21dd579c55b4f32f20f33ee2a
Serah <sarahnjoroge2015@gmail.com>
2022-06-02 06:47:23 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main-print the strings in the puts function
+ * Description: c programming
+ * Return:0
+ */
+int main(void)
+{
+	puts("/"programming is like building a multilingual puzzle");
+	return (0);
+}
