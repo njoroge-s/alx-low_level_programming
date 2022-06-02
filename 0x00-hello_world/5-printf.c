79cf773dc015abcb6fcdcf98f70265b60c06fd40
Serah <sarahnjoroge2015@gmail.com>
2022-06-02 10:32:04 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - printing a function
+ * Description: Printing "with proper grammer"
+ * Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art");
+return (0);
+}
