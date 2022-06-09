4c5265a50922ae1742c80b9478be9a4438e0c4e6
Serah <sarahnjoroge2015@gmail.com>
2022-06-09 13:19:50 -0700

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,16 +15,22 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0)
+		{
 			printf("Fizz");
-		else if (i % 5 == 0)
+		} else if (i % 5 == 0)
+		{
 			printf("Buzz");
-		else if (i % 3 == 0 && i % 5 == 0)
+		} else if (i % 3 == 0 && i % 5 == 0)
+		{
 			printf("FizzBuzz");
-		else
+		} else if (i == 1)
+		{
 			printf("%d", i);
-		if (i != 100)
-			putchar(' ');
+		} else
+		{
+			printf(" %d", i);
+		}
 	}
-	putchar('\n');
+	printf("\n");
 	return (0);
 }
