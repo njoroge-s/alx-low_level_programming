f144e93184610a2fa0c8261dc650867886966052
Serah <sarahnjoroge2015@gmail.com>
2022-06-13 10:13:54 -0700

    Fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,19 +3,18 @@
 /**
  * print_rev - prints a string in reverse
  * @s: the sting to be printed
+ * Return: 0
  */
 void print_rev(char *s)
 {
-	int k;
-	k = 0;
-	while (s[k] != '\0')
+	int live;
+
+	for (live = 0; != '\0'; live++)
 	{
-		k++;
 	}
-	while (k >= 0)
+	for (live = live - 1; live >= 0; live--)
 	{
-		putchar(str[k]);
-		k--;
+		_putchar(s[live]);
 	}
-	putchar('\n');
+	_putchar('\n');
 }
