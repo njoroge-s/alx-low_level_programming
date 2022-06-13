521ea71fed0122e8dd7920cdfa34311cd8475935
Serah <sarahnjoroge2015@gmail.com>
2022-06-13 03:56:26 -0700

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - prints out every character on the string
+ * @str: input string
+ */
+void puts2(char *str)
+{
+	int i;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
