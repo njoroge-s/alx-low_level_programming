13e37648d0f8371f961f80859e0bf90c14c09d57
Serah <sarahnjoroge2015@gmail.com>
2022-06-13 16:28:59 -0700

    Fix

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,13 +7,12 @@
  */
 void puts2(char *str)
 {
-	int len = 0; i = 0;
+	int x = strlen(str);
+	int i;
 
-	while (str[len] != '\0')
-		len++;
-
-	len -= 1;
-
-	for (; i <= len; i += 2)
-		_putchar('\n');
+	for (i = 0; i < x; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	putchar('\n');
 }
