b074ee904de1b48cbfeb1edbcb885152d618d0a8
Serah <sarahnjoroge2015@gmail.com>
2022-06-13 04:05:35 -0700

    fix

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * puts2 - prints out every character on the string
  * @str: input string
---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * _puchar - writes a character to standard output
+ * @c: character to print
+ * Return: 1 on success, -1 on error
+ */
+int _putchar(char c)
+{
+	return(write(1, &c, 1));
+}
