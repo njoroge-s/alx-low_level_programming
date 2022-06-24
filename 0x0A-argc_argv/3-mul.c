0565c81b26089365bd5177a548fbe35e4f82d007
njoroge-s <sarahnjoroge2015@gmail.com>
2022-06-24 15:11:29 +0300

    argc

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,24 +3,72 @@ s (23 sloc)  447 Bytes
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - multiplies two numbers and prints the result
- * @argc: argument count must be exactly two numbers
- * @argv: argument vector
- * Return: 0 on success, 1 on failure
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
+int _atoi(char *s)
+{
+	int i, d, n, len, f, digit;
+
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+
+	if (f == 0)
+		return (0);
+
+	return (n);
+}
+
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
  */
 int main(int argc, char *argv[])
 {
-	int res1, res2;
+	int result, num1, num2;
 
-	res1 = res2 = 0;
-	if (argc == 3)
+	if (argc < 3 || argc > 3)
 	{
-		res1 = atoi(argv[1]);
-		res2 = atoi(argv[2]);
-		printf("%d\n", res1 * res2);
-		return (0);
-	}
-	else
 		printf("Error\n");
-	return (1);
+		return (1);
+	}
+
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
+	result = num1 * num2;
+
+	printf("%d\n", result);
+
+	return (0);
 }
