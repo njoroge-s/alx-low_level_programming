24e0a18bc4d82ec9f43d99d9e175fc747f5238bf
njoroge-s <sarahnjoroge2015@gmail.com>
2022-09-05 23:21:04 -0700

    bug fix

--- 0x18-dynamic_libraries/arithmetic.h
+++
@@ -1,9 +0,0 @@
-#ifndef _ARITHMETIC_H_
-#define _ARITHMETIC_H_
-int getSum(int num1, int num2);
-int getDifference(int num1, int num2);
-int getProduct(int num1, int num2);
-float getQuotient(int num1, int num2);
-int getModulo(int num1, int num2);
-
-#endif /* _ARITHMETIC_H_ */
